83459163f862ee056db229662210c39d2c1ba2b4
Dan <55022836+gsmj@users.noreply.github.com>
2024-12-11 14:43:06 +0300

    Update callbacks.py

--- driftcounter/callbacks.py
+++ driftcounter/callbacks.py
@@ -1,7 +1,7 @@
 from pysamp import register_callback
 from pysamp.event import event
 
-def register() -> None:
+def register_drift_callbacks() -> None:
     register_callback("OnPlayerDriftStart", "i")
     register_callback("OnPlayerDriftUpdate", "iiiiff")
     register_callback("OnPlayerDriftEnd", "iiii")
