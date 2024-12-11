661dea5c3880839279298f8b723d572a4bfbcfe6
Dan <55022836+gsmj@users.noreply.github.com>
2024-12-11 14:42:43 +0300

    Update drift.py
    removed Drift class

--- driftcounter/drift.py
+++ driftcounter/drift.py
@@ -1,19 +1,18 @@
 from pysamp.event import event
 
 
-class Drift:
-    ...
+@event("OnPlayerDriftStart")
+def on_start(player_id: int):
+    return (Player(player_id), )
 
-    @event("OnPlayerDriftStart")
-    def on_start(cls, player_id: int) -> None:
-        return (Player(player_id), )
 
-    @event("OnPlayerDriftUpdate")
-    def on_update(cls, player_id: int, value: int, combo: int, flag_id: int, distance: float, speed: float) -> None:
-        return (Player(player_id), value, combo, flag_id, distance, speed)
+@event("OnPlayerDriftUpdate")
+def on_update(player_id: int, value: int, combo: int, flag_id: int, distance: float, speed: float):
+    return (Player(player_id), value, combo, flag_id, distance, speed)
 
-    @event("OnPlayerDriftEnd")
-    def on_end(cls, player_id: int, value: int, combo: int, reason: int):
-        return (Player(player_id), value, combo, reason)
+
+@event("OnPlayerDriftEnd")
+def on_end(player_id: int, value: int, combo: int, reason: int):
+    return (Player(player_id), value, combo, reason)
 
 from pysamp.player import Player # noqa
